6fd32e567d5281e3b34ba8e95de9401dcc855b52
XavierJiezou <xuechaozou@foxmail.com>
2024-10-20 16:14:06 +0800

    l8数据集，dinov2,head,full训练配置

--- rein/datasets/__init__.py
+++ rein/datasets/__init__.py
@@ -3,11 +3,13 @@ from .gf12ms_whu_gf1 import GF12MSWHUGF1Dataset
 from .gf12ms_whu_gf2 import GF12MSWHUGF2Dataset
 from .cloudsen12_high_l1c import CLOUDSEN12HIGHL1CDataset
 from .cloudsen12_high_l2a import CLOUDSEN12HIGHL2ADataset
+from .l8_biome import L8BIOMEDataset
 
 __all__ = [
     "HRCWHUDataset",
     "GF12MSWHUGF1Dataset",
     "GF12MSWHUGF2Dataset",
     "CLOUDSEN12HIGHL1CDataset",
-    "CLOUDSEN12HIGHL2ADataset"
+    "CLOUDSEN12HIGHL2ADataset",
+    "L8BIOMEDataset",
 ]
