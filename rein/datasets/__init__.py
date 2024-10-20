f6e9ddac0c7c942acd0ba1e331e9e748027a89e2
XavierJiezou <xuechaozou@foxmail.com>
2024-10-20 15:38:15 +0800

    增加l1c数据集及其配置

--- rein/datasets/__init__.py
+++ rein/datasets/__init__.py
@@ -1,5 +1,11 @@
 from .hrc_whu import HRCWHUDataset
 from .gf12ms_whu_gf1 import GF12MSWHUGF1Dataset
 from .gf12ms_whu_gf2 import GF12MSWHUGF2Dataset
+from .cloudsen12_high_l1c import CLOUDSEN12HIGHL1CDataset
 
-__all__ = ["HRCWHUDataset","GF12MSWHUGF1Dataset","GF12MSWHUGF2Dataset"]
+__all__ = [
+    "HRCWHUDataset",
+    "GF12MSWHUGF1Dataset",
+    "GF12MSWHUGF2Dataset",
+    "CLOUDSEN12HIGHL1CDataset",
+]
