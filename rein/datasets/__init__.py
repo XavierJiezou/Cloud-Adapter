e81475325f39e2f29dba1e6947f54b8f3f513261
XavierJiezou <xuechaozou@foxmail.com>
2024-10-20 15:58:35 +0800

    增加l2a数据集，dinov2,full,head训练配置

--- rein/datasets/__init__.py
+++ rein/datasets/__init__.py
@@ -2,10 +2,12 @@ from .hrc_whu import HRCWHUDataset
 from .gf12ms_whu_gf1 import GF12MSWHUGF1Dataset
 from .gf12ms_whu_gf2 import GF12MSWHUGF2Dataset
 from .cloudsen12_high_l1c import CLOUDSEN12HIGHL1CDataset
+from .cloudsen12_high_l2a import CLOUDSEN12HIGHL2ADataset
 
 __all__ = [
     "HRCWHUDataset",
     "GF12MSWHUGF1Dataset",
     "GF12MSWHUGF2Dataset",
     "CLOUDSEN12HIGHL1CDataset",
+    "CLOUDSEN12HIGHL2ADataset"
 ]
